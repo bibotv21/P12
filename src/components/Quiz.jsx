d343b104409b4dc649aee31fe110dca4f5fe7caf
bibotv21 <piter53553@gmail.com>
2025-10-16 23:57:47 +0700

    implement basic quiz

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import QUESTIONS from "../questions.js";
+
+export default function Quiz() {
+  const [activeUserAnswer, setActiveUserAnswer] = useState([]);
+
+  const activeQuestionIndex = activeUserAnswer.length;
+
+  function handleSelectedAnswer(answer) {
+    setActiveUserAnswer((prevAnswer) => {
+      return [...prevAnswer, answer];
+    });
+  }
+
+  return (
+    <div id="question">
+      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+
+      <ul id="answers">
+        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          <li key={answer} className="answer">
+            <button onClick={() => handleSelectedAnswer(answer)}>
+              {answer}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
